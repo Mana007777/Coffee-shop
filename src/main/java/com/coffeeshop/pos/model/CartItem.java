416413b91ea83e03c84e3388d4b63c3846aa74fd
mana007777 <e7ifubbb@gmail.com>
2026-03-18 03:36:11 +0300

    CartItem Model

---
+++ src/main/java/com/coffeeshop/pos/model/CartItem.java
@@ -0,0 +1,42 @@
+package com.coffeeshop.pos.model;
+
+public class CartItem {
+
+    private Product product;
+    private int quantity;
+    private double subtotal;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.subtotal = product.getPrice() * quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+        recalculateSubtotal();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        recalculateSubtotal();
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    private void recalculateSubtotal() {
+        if (product != null) {
+            this.subtotal = product.getPrice() * quantity;
+        }
+    }
+}
