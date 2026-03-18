29289bca7bd490e3f2508d48a2ac3bd2fac70f1d
mana007777 <e7ifubbb@gmail.com>
2026-03-18 03:28:08 +0300

    creating models

---
+++ src/main/java/com/coffeeshop/pos/model/Category.java
@@ -0,0 +1,4 @@
+package com.coffeeshop.pos.model;
+
+public class Category {
+}
---
+++ src/main/java/com/coffeeshop/pos/model/User.java
@@ -0,0 +1,17 @@
+package com.coffeeshop.pos.model;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(){}
+    public User(int id, String username, String password, String role){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
