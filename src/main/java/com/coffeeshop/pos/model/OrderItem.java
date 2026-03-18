644a2a9a60a9cdfc9003f3825e87e4b00ca53475
mana007777 <e7ifubbb@gmail.com>
2026-03-18 03:39:56 +0300

    Create CartItem Model

---
+++ src/main/java/com/coffeeshop/pos/model/OrderItem.java
@@ -0,0 +1,81 @@
+package com.coffeeshop.pos.model;
+
+public class OrderItem {
+
+    private int id;
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private double unitPrice;
+    private double subtotal;
+    private String note;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(int orderId, int productId, int quantity, double unitPrice, double subtotal, String note) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.subtotal = subtotal;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
