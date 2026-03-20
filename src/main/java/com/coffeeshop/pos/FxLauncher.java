1993d2f5a43d45aabc6606905e6fc05821936285
mana007777 <e7ifubbb@gmail.com>
2026-03-20 04:49:51 +0300

    kurdish font

--- src/main/java/com/coffeeshop/pos/FxLauncher.java
+++ src/main/java/com/coffeeshop/pos/FxLauncher.java
@@ -6,11 +6,16 @@ import com.coffeeshop.pos.ui.LoginView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.text.Font;
 public class FxLauncher extends Application {
 
     @Override
     public void start(Stage stage) {
+
+
+        Font.loadFont("file:/usr/share/fonts/truetype/noto/NotoSansArabic-Regular.ttf", 14);
+        Font.loadFont("file:/usr/share/fonts/truetype/noto/NotoSansArabic-Bold.ttf", 14);
+
         DatabaseInitializer.initializeDatabase();
         DataSeeder.seedData();
 
